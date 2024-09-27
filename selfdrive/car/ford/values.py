a6557633ac846a22435418e06cbe5e1ff5f43e90
SusanYu25 <yuhong299@gmail.com>
2024-09-27 10:59:18 +0800

    Update values.py

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -112,7 +112,7 @@ class CAR(Platforms):
   )
   FORD_EDGE_MK2 = FordPlatformConfig(
     [FordCarDocs("Ford Edge 2022")],
-    CarSpecs(mass=1933, steerRatio=15.3, wheelbase=2.824),
+    CarSpecs(mass=1933, steerRatio=20.3, wheelbase=2.824),
     flags=FordFlags.ALT_STEER_ANGLE,
   )
   FORD_ESCAPE_MK4 = FordPlatformConfig(
